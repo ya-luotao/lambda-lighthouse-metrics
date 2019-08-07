5d5a71c67d3dfc75b8fc558fbf33f2de616d744a
luotao <luotao.ruby@gmail.com>
2019-08-07 15:42:58 +0800

    Add func initialize and aws iam role.

--- handler.js
+++ handler.js
@@ -16,3 +16,7 @@ module.exports.hello = async event => {
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
   // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
 };
+
+module.exports.initialize = (evt, ctx, cbk) => {
+
+}
